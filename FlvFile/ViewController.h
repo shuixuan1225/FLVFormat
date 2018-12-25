3dfb26eac0d27b69a86edd302b103bebdf79e498
kirk <1310134735@qq.com>
2018-12-25 18:50:23 +0800

    Initial Commit

---
+++ FlvFile/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FlvFile
+//
+//  Created by kirk on 2018/12/25.
+//  Copyright © 2018 kirk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ FlvFile/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  FlvFile
+//
+//  Created by kirk on 2018/12/25.
+//  Copyright © 2018 kirk. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
